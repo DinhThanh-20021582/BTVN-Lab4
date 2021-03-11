2238fefee3e9d6118764b011726c7601a39a2ff7
Phùng Đình Thành <78190218+DinhThanh-20021582@users.noreply.github.com>
2021-03-12 00:27:28 +0700

    fouth commit

---
+++ A.2,a.cpp
@@ -0,0 +1,20 @@
+ 
+#include<iostream>
+using namespace std;
+
+int main (int argc, char* argv[]) {
+	int a[] = {4, 5, 2, 1, 3};
+	int n=2;
+	bool b=false;
+	for(int i=0;i<sizeof(a) / sizeof(a[0]);i++)
+	{
+		if(a[i] == n)
+	{
+		b=true;
+		break;
+	}
+	}
+	if(b==true) cout << "Có";
+	else cout << "Không";
+	return 0;
+}
